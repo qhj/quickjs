2d4389be32916c22a38e89451e729f9a361d68c2
qhj <me@qhj.moe>
2024-01-06 01:32:42 +0800

    nix develop

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self , nixpkgs ,... }:
+  let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}" = {
+      default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          fish
+        ];
+
+        shellHook = with pkgs; ''
+          exec fish
+        '';
+      };
+    };
+  };
+}
